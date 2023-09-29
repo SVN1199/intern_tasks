7fce8169e0487db005da6f2926056a1982631522
SVN1199 <vasanthnov1199@gmail.com>
2023-09-29 22:42:25 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const dotenv = require('dotenv').config();
 const config = require('./config/db')
@@ -11,6 +12,19 @@ app.use(express.urlencoded({extended : false}))
 
 app.use('/api/profile', profileRouter)
 app.use('/api/users', userRouter)
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 app.listen(port, ()=>{
     console.log(`Server Running on ${port}`)
